324310a0710ff0a09ecee73f52b2243efc20f028
Giovan Herrera <gio2020herrera@gmail.com>
2024-10-20 22:14:02 -0500

    declaracion de variables

--- src/PromedioVentas.java
+++ src/PromedioVentas.java
@@ -1,2 +1,7 @@
 public class PromedioVentas {
+    static double promVentasAnuales = 0;
+    static String[] nombresVendedores;
+    static int n;
+    static double[] ventasTrismetresVendedores;
+    static double[] promPorVendedor;
 }
