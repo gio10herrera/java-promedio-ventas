b28d10716468ef43a5d1022c0c7537d88c6e2118
Giovan Herrera <gio2020herrera@gmail.com>
2024-10-20 22:21:36 -0500

    variables instanciadas

--- src/PromedioVentas.java
+++ src/PromedioVentas.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class PromedioVentas {
     static double promVentasAnuales = 0;
     static String[] nombresVendedores;
     static int n;
-    static double[] ventasTrismetresVendedores;
+    static double[][] ventasTrismetresVendedores;
     static double[] promPorVendedor;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Numero de vendedores: ");
+        n = input.nextInt();
+        nombresVendedores = new String[n];
+        ventasTrismetresVendedores = new double[n][4];
+        promPorVendedor = new double[n];
+
+    }
 }
